928900e78b04f7d7f92ecf7554788d847feeda19
raymondsquared <bolesray@outlook.com>
2024-07-16 23:00:39 +1000

    feat: create a component
    https://backstage.io/docs/getting-started/create-a-component

--- backstage/packages/backend/src/index.ts
+++ backstage/packages/backend/src/index.ts
@@ -20,10 +20,11 @@ backend.add(import('@backstage/plugin-auth-backend'));
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 // See https://backstage.io/docs/auth/guest/provider
-backend.add(import('@backstage/plugin-auth-backend-module-github-provider'))  
+backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend/alpha'));
+backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
